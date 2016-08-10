d9092837bfba075ee3e3c426bf449469afb0fae2
Rodrigo Morais <morais.rm@gmail.com>
2016-08-10 17:31:28 -0300

    Adjusting project to use React.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: './dist',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
